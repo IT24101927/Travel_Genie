321b68a4fac5f5180688b4ad22083300e9ba6daf
IT24100853 <IT24100853@MY.SLIIT.LK>
2026-03-22 16:57:58 +0530

    Delete Frontend directory

--- Frontend/src/config/api.js
+++
@@ -1,4 +0,0 @@
-const rawBaseUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000'
-
-export const API_BASE_URL = rawBaseUrl.replace(/\/$/, '')
-export const API_BASE = `${API_BASE_URL}/api`
