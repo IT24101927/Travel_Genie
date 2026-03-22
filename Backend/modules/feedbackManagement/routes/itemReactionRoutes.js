37db9085a9cad87b6cb276f2f5353c0cf5a7354c
daniduwijesinghe <daniduwijesinghe11@gmail.com>
2026-03-22 17:13:32 +0530

    Delete Backend directory

--- Backend/modules/feedbackManagement/routes/itemReactionRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const { protect } = require('../../../middleware/auth');
-const ctrl = require('../controllers/itemReactionController');
-
-router.post('/', protect, ctrl.toggleReaction);
-router.get('/item/:itemId', protect, ctrl.getByItem);
-router.get('/item/:itemId/me', protect, ctrl.getMyReaction);
-
-module.exports = router;
